4de95a576739688e25a5c8e1a6346c9e19543ea0
hossamhosss <153024340+hossamhosss@users.noreply.github.com>
2024-05-15 13:04:38 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+class Config:
+    API_ID = 19312827
+    API_HASH = "84da7f08e87849853b2fa6728e4192a2"
+    TOKEN = "5475135573:AAEoA-AyByxQWmv0ctg_d2ltzfmNScJ67Vk"  
+    START_PIC = "https://telegra.ph/file/d167a93750349bbddd497.jpg" 
+    CHAT = "-1001337478310"    
